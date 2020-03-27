7a09320c11e04adf545150cc8b8c7a9e8593f423
Allen <allen.cary@gmail.com>
2020-03-27 09:51:07 -0500

    new

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.example.java;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello from IDEA!");
+	    System.out.println("Hello from me!");
         String aString = "Allen";
         System.out.println("Your name is " + aString);
     }
