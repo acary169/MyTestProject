143ddbfdc70278a6b52c5fbcb02daaa35ae8a5b4
Allen <allen.cary@gmail.com>
2020-03-27 13:01:15 -0500

    new

--- src/com/examples/java/Main.java
+++ src/com/examples/java/Main.java
@@ -1,9 +1,12 @@
 package com.examples.java;
 
+
 public class Main {
+
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hi new project");
+
     }
 }
